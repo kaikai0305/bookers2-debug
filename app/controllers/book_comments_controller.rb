6c985f23e8655bc9bdb6c70d9885090e7dd5443f
kaikai0305 <kaikai030552@gmail.com>
2022-04-10 02:13:28 +0000

    Finish 応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,12 +5,12 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    render :book_comments
+    render :index
   end
 
   def destroy
      BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-     render :book_comments
+     render :index
   end
 
   private
